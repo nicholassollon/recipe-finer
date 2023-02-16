75f4869b6b3db2077c441f0c4c387e5573569724
Nick Sollon <genericjoke@gmail.com>
2023-02-16 15:45:11 -0500

    List of recipes and recipe indvidual pages working

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,38 +1,35 @@
 import React from 'react'
-import {Link} from "react-router-dom"
-function Nav(){
+import { Link } from "react-router-dom"
+function Nav() {
 
-    function navigate(e) {
-        window.history.pushState({}, "", e);
-      }
 
-    return(
+    return (
         <div className='Nav'>
             <section class="wrapper">
                 <div class="top">Recipe Findr</div>
                 <div class="bottom" aria-hidden="true">Recipe Findr</div>
             </section>
-            
+
             <input type="checkbox" id="ham-menu" />
             <label for="ham-menu">
-            <div class="hide-des">
-                <span class="menu-line"></span>
-                <span class="menu-line"></span>
-                <span class="menu-line"></span>
-                <span class="menu-line"></span>
-                <span class="menu-line"></span>
-                <span class="menu-line"></span>
-            </div>
+                <div class="hide-des">
+                    <span class="menu-line"></span>
+                    <span class="menu-line"></span>
+                    <span class="menu-line"></span>
+                    <span class="menu-line"></span>
+                    <span class="menu-line"></span>
+                    <span class="menu-line"></span>
+                </div>
             </label>
-        <div class="full-page-green"></div>
-        <div class="ham-menu">
-            <ul class="centre-text bold-text">
-                <Link to="/home">Home</Link>
-                <Link to="/recipes">Recipes</Link>
-                <Link to="/form">Submit Your Recipe</Link>
-            </ul>
+            <div class="full-page-green"></div>
+            <div class="ham-menu">
+                <ul class="centre-text bold-text">
+                    <Link to="/home">Home</Link>
+                    <Link to="/recipes">Recipes</Link>
+                    <Link to="/form">Submit Your Recipe</Link>
+                </ul>
+            </div>
         </div>
-    </div>
 
 
     )
--- src/components/RecipieCollection.js
+++ src/components/RecipieCollection.js
@@ -1,12 +1,13 @@
 import React from "react";
 import RecipieCard from "./RecipieCard"
 
-function RecipieCollection({recipies}) {
-    const recipieCards = recipies?.map((recipie) => { return (<RecipieCard recipie={recipie} key= {recipie.id}/>)} )
+function RecipieCollection({ recipies }) {
+
+    const recipieCards = recipies?.map((recipie) => { return (<RecipieCard recipie={recipie} key={recipie.id} id={recipie.id} />) })
 
 
     return (
-        <div> {recipieCards}</div>
+        <div class="cardList"> {recipieCards}</div>
     )
 }
 
