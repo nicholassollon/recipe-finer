cb25254b38b362428611a5c89e0837dda4dae3ed
Nickita Danilovich <nickitadanilovich@gmail.com>
2023-02-17 12:07:16 -0500

    search css and recipiepage css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -36,29 +36,7 @@ function Nav() {
             </div>
 
         </div>
-      </section>
-
-      <input type="checkbox" id="ham-menu" />
-      <label for="ham-menu">
-        <div class="hide-des">
-          <span class="menu-line"></span>
-          <span class="menu-line"></span>
-          <span class="menu-line"></span>
-          <span class="menu-line"></span>
-          <span class="menu-line"></span>
-          <span class="menu-line"></span>
-        </div>
-      </label>
-      <div class="full-page-green"></div>
-      <div class="ham-menu">
-        <ul class="centre-text bold-text">
-          <Link to="/home">Home</Link>
-          <Link to="/recipes">Recipes</Link>
-          <Link to="/form">Submit Your Recipe</Link>
-        </ul>
-      </div>
-    </div>
-  );
+    )
 }
 
 export default Nav;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import '../Search.css'
 function Search({search, setSearch, setIsChecked}) {
     return(
-        <div className="Search">
-            <input type="text" placeholder="Search A Recipe" className="prompt" value={search} onChange= {(e) => setSearch(e.target.value)}  />
+        <div className="search">
+            <input class="search2" type="text" placeholder="Search A Recipe" className="prompt" value={search} onChange= {(e) => setSearch(e.target.value)}  />
         </div>
         // <div className='Search'>
         //     <input type="text" placeholder="Search" value={search} onChange= {(e) => setSearch(e.target.value)/>
